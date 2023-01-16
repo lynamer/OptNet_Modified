5c55f884d7f1b066779ff04cf813b041dc7e2438
pbkkkkk <pbk523@126.com>
2023-01-16 16:45:09 +0800

    test some instances for Ours

--- classification/test.py
+++ classification/test.py
@@ -21,10 +21,11 @@ from torch.utils.data import DataLoader
 
 import os
 import sys
+import copy
 import math
 import numpy as np
 import shutil
-
+import random
 import setproctitle
 
 import densenet
@@ -39,10 +40,9 @@ class MIPSolver(nn.Module):
         super().__init__()
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         n = data.var_nodes.size(0)
+        print(f"mu: {mu}")
         # print('n: ', n)
-        Q = torch.eye(n)
-        # Q = torch.zeros([n, n])
-        Q = Q * mu
+        self.no_eq_cons_flag = False
         
         eq_cons_idx = []
         eq_cons_val = []
@@ -90,11 +90,13 @@ class MIPSolver(nn.Module):
                 neq_cons_idx.append([cons_index_map[con_idx], var_idx]) # constaint index
                 neq_cons_val.append(data.edge_attr[i]) # corresponding A
         # there is no equality constaints in the problem. For the sake of 0.
-        if not eq_cons_list: 
-            eq_cons_idx = torch.tensor([[0], [0]]).to(torch.long)
-            eq_cons_val = torch.tensor([0]).to(torch.float32)
+        if not eq_cons_list:
+            self.no_eq_cons_flag = True
+            eq_cons_idx = torch.tensor([[0], [n]]).to(torch.long)
+            eq_cons_val = torch.tensor([1]).to(torch.float32)
             eq_cons_b = torch.tensor([0]).to(torch.float32)
             n_eq_conditions = 1
+            n = n + 1
         else:
             eq_cons_idx = torch.tensor(np.array(eq_cons_idx).T).to(torch.long)
             eq_cons_val = torch.tensor(eq_cons_val).to(torch.float32)
@@ -105,7 +107,7 @@ class MIPSolver(nn.Module):
 
         n_conditions = len(neq_cons_list)
         # Here we suppose all of the variables have upper and lower bounds. so we add 1000 * 2 constraints to the matrix to make G become a 27000 * 1000 matrix
-        for i in range(n): 
+        for i in range(n - self.no_eq_cons_flag): 
             lb = data.var_nodes[i][2]
             ub = data.var_nodes[i][3]
             neq_cons_idx.append([n_conditions, i])
@@ -125,10 +127,16 @@ class MIPSolver(nn.Module):
 
         c = []
         bin = []
-        for i in range(n):
+        for i in range(n - self.no_eq_cons_flag):
             bin.append(data.var_nodes[i][0]) # whether binary
             c.append(data.var_nodes[i][1])
         # print([f'{c[i]} * x_{i}' for i in range(n)], sep="+", end='')
+        if self.no_eq_cons_flag:
+            bin.append(0)
+            c.append(0)
+        Q = torch.eye(n)
+        # Q = torch.zeros([n, n])
+        Q = Q * mu
         self.Q = Q.to(device)
         self.G = G.to(device)
         self.h = h.to(device)
@@ -144,6 +152,7 @@ class MIPSolver(nn.Module):
 
     def forward(self, y_0):
         nBatch = 1
+        
         # Here batch is not for any use, just to meet with newlayer's code.
         Q = self.Q.unsqueeze(0).expand(nBatch, self.Q.size(0), self.Q.size(1)).double()
         G = self.G.to_dense()
@@ -154,14 +163,16 @@ class MIPSolver(nn.Module):
         A = A.unsqueeze(0).expand(nBatch, self.A.size(0), self.A.size(1)).double()
         
         b = self.b.unsqueeze(0).expand(nBatch, self.b.size(0)).double()
-        
+        print(f"lambda: {self.lam}")
+        print(f"L: {self.L}")
         p = []
         for i in range(len(y_0)):
             temp = self.c[i] 
             if self.bin[i]:                                         # is binary variable
                 temp = temp + self.lam * (1 - self.L * math.pow(y_0[i], self.L-1)) # add DC constraints, p = c + 1 - L (z_i)^L     else p = c
             p.append(temp)
-            
+        if self.no_eq_cons_flag:
+            p.append(0)
         p = torch.tensor(p).to(torch.float32)
         p = p.to(self.device)
         p = p.unsqueeze(0).expand(nBatch, p.size(0)).double()
@@ -171,24 +182,28 @@ class MIPSolver(nn.Module):
         print(f"h.size: {h.size()}")
         print(f"A.size: {A.size()}")
         print(f"b.size: {b.size()}")
-        # x = diff(verbose=False)(Q, p, G, h, A, b).float()
+        x = diff(verbose=False)(Q, p, G, h, A, b).float()
         
-        x = QPFunction(verbose=True)(Q, p, G, h, A, b).float()
+        # for i in range(len(x)):
+            
+        #     if self.bin[i]: 
+        #         result[i] = round(result[i])# is binary variable
         # print( G @ x.T - h)
         # print( A @ x.T - b)
-
-        return x
+        # x = QPFunction(verbose=True)(Q, p, G, h, A, b).float()
+        if self.no_eq_cons_flag:
+            return x[:, :-1]
+        else:
+            return x
     
 
 
 def solve_MIP(data):
     mu = 1e-3
     L = 2
-    lam = 50
+    lam = 5
     opt_prob = MIPSolver(data, mu=mu, lam=lam)
-    result = opt_prob(data.y)
-    
-    return result
+    return opt_prob(data.y)
     
 # def test_diff():
 #     n = 10
@@ -207,7 +222,14 @@ if __name__=='__main__':
     # data = torch.load(os.path.join("instances", "10r5c_eq_2.pt"))
     # data = torch.load(os.path.join("instances", "10r5c_eq_3.pt"))
     # data = torch.load(os.path.join("instances", "10r5c_eq_4.pt"))
-    data = torch.load(os.path.join("instances", "10r5c_neq.pt"))
+    # instance_path = os.path.join("instances","tiny_10r_5c_0.55d", "instance_2.pt")
+    instance_path = os.path.join("instances","tiny_1000r_500c_0.05d", "instance_3.pt")
+    # instance_path = os.path.join("instances","10r5c_neq.pt")
+    # instance_path = os.path.join("instances","instance_1.pt")
+    # instance_path = os.path.join("instances", "10r5c_eq_4.pt")
+    # instance_path = os.path.join("instances", "3r_3c.pt")
+    data = torch.load(instance_path)
+    print(f"runing instance :{instance_path}")
     x = solve_MIP(data)
     # print(x)
     # print(x.size())
@@ -218,6 +240,6 @@ if __name__=='__main__':
     for i in range(len(data.var_nodes)):
         res = data.var_nodes[i][1] * x[0][i]
         sum = sum + res
-    print(sum)
-    print(data.obj)
+    print(f"final objective: {sum}")
+    print(f"expected objective: {data.obj}")
     # test_diff()
